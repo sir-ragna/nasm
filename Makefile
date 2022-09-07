fe52cfe223af69356b00d685477fc06a1d90057e
Robbe Van der Gucht <sir.ragna@gmail.com>
2022-09-07 23:02:46 +0200

    refactor loops and link directly

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ all:    build
 
 build:
 	nasm -X gnu -f elf64 -g main.asm -o main.o
-	gcc -Wall main.o -no-pie -o main
-#	ld -o main -m elf_x86_64 main.o
+#	gcc -Wall main.o -no-pie -o main
+	ld -o main -m elf_x86_64 main.o
 # nasm
 # -O0 disable optimisation
 # -X gnu
--- main.asm
+++ main.asm
@@ -15,7 +15,7 @@ yourname    resb    512 ; hope you name isn't longer than 512 bytes
 
     section .text
     default rel
-    global  main
+    global  _start
 ;    extern puts
 ; Arguments
 ; rdi   rsi   rdx   r10   r8    r9 
@@ -23,8 +23,8 @@ yourname    resb    512 ; hope you name isn't longer than 512 bytes
 ; rax   
 ; char *
 
-main:
-            push    rbp             ; create a stack frame to align to 16-bit
+_start:
+            ;push    rbp             ; create a stack frame to align to 16-bit
 
 ; https://cs50.harvard.edu/x/2022/psets/1/mario/less/
 ; Create a triangle
@@ -42,34 +42,37 @@ main:
             mov     rcx, 0          ; int i = 0
 .nextline:  ; for(int i = 0; i < 8; i++)
 
+            cmp     rcx, r10        ; for (i < height)
+            jge     .exitouter      ; i >= height  exit
+
             ;; start of the first inner loop
             mov     rbx, r10        ; int j = height - i
             sub     rbx, rcx        ; 
 
-
-            cmp     rbx, 1          ; for (j > 1)
-            jle      .exitspace     ;       
 .nextspace: ; for(int j = height - i; j > 1; j--)
             
+            cmp     rbx, 1          ; for (j > 1)
+            jle     .exitspacel            
 
             ; print a space
-            push    '*'             ; push a space on the stack
+            push    ' '             ; push a space on the stack
             mov     rax, rsp        ; put the address to the stack in rax
             mov     rdi, 1          ; int len = 1
             call    lprint          ; print our space character
             pop     rax             ; remove from the stack
             
             dec     rbx             ; j--
-            cmp     rbx, 1          ; for (j > 1)
-            jg      .nextspace      ;       
+            jmp     .nextspace      ; } // endloop
             
-.exitspace:
+.exitspacel:
 
             ;; start of second inner loop
             mov     rbx, rcx
             inc     rbx             ; int j = i + 1
 .nexthash:  ; for(int j = i + 1; j > 0; j--)
 ;            jmp     .hashcon        ; jump to the hash condition first
+            cmp     rbx, 0          ; for (j > 0)
+            jle      .exithashl     
 
             ; print a hash
             push    '#'             ; push a hash on the stack
@@ -79,28 +82,30 @@ main:
             pop     rax             ; remove from the stack
             
             dec     rbx             ; j--
-.hashcon:   cmp     rbx, 0          ; for (j > 0)
-            jg      .nexthash       ;       
+            jmp     .nexthash
 
-            push    byte 10
-            mov     rax, rsp        ; put the address to the stack in rax
+.exithashl: ; exit hash loop
+
+            push    byte 10         ; newline character
+            mov     rax, rsp        ; addr to newline char
             mov     rdi, 1          ; int len = 1
-            call    lprint          ; print our space character
+            call    lprint          ; print our newline character
             pop     rax             ; remove from the stack
 
             inc     rcx             ; i++
-            cmp     rcx, r10        ; for (i < height)
-            jl      .nextline
+            jmp     .nextline       ; // end outer loop
 
             ; mov     rdi, hellow
             ; call    puts wrt ..plt
 
-            pop rbp
-            ret
-            ; -- exit --
-            ; mov     rdi, 0          ; exit code
-            ; mov     rax, 60         ; exit syscall
-            ; syscall
+.exitouter: 
+
+            ; pop rbp
+            ; ret
+            ;-- exit --
+            mov     rdi, 0          ; exit code
+            mov     rax, 60         ; exit syscall
+            syscall
 
 section .note.GNU-stack
 ; gets rid of annoying GCC warning that the stack is executable
